55303b580a1ddd56e1bea59b918ed0d465480c26
valentina-vallejos-podio <valentinavallejos2022@gmail.com>
2022-08-10 20:55:08 -0300

    make merccadoLiebre

---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require("express")
+const donetv=require("dotenv").config();
+const path= require("path")
+const app=express()
+
+app.use(express.static('public'))
+
+app.listen(process.env.PORT,()=>{
+  console.log("servidor funcionando en el puerto" + process.env.PORT);
+});
+
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname,"/views/home.html"))
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname,"/views/register.html"))
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname,"/views/login.html"))
+});
